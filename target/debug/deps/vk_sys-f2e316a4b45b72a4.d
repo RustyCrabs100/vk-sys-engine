3335ca43145288a70a91c5479a5c52f2fa6ad7be
Rusty Crab <rusty.crabs100@gmail.com>
2025-06-01 18:25:14 -0400

    Fixed visibility error and removed unnecessary directory

---
+++ target/debug/deps/vk_sys-f2e316a4b45b72a4.d
@@ -0,0 +1,7 @@
+C:\Users\metee\Downloads\Rust_Coding\repo_clone\target\debug\deps\libvk_sys-f2e316a4b45b72a4.rmeta: C:\Users\metee\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\vk-sys-0.7.0\src\lib.rs
+
+C:\Users\metee\Downloads\Rust_Coding\repo_clone\target\debug\deps\libvk_sys-f2e316a4b45b72a4.rlib: C:\Users\metee\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\vk-sys-0.7.0\src\lib.rs
+
+C:\Users\metee\Downloads\Rust_Coding\repo_clone\target\debug\deps\vk_sys-f2e316a4b45b72a4.d: C:\Users\metee\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\vk-sys-0.7.0\src\lib.rs
+
+C:\Users\metee\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\vk-sys-0.7.0\src\lib.rs:
---
+++ target/debug/deps/windows_targets-2a42829cfd35da98.d
@@ -0,0 +1,8 @@
+C:\Users\metee\Downloads\Rust_Coding\repo_clone\target\debug\deps\libwindows_targets-2a42829cfd35da98.rmeta: C:\Users\metee\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\windows-targets-0.53.0\src\lib.rs C:\Users\metee\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\windows-targets-0.53.0\src\../readme.md
+
+C:\Users\metee\Downloads\Rust_Coding\repo_clone\target\debug\deps\libwindows_targets-2a42829cfd35da98.rlib: C:\Users\metee\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\windows-targets-0.53.0\src\lib.rs C:\Users\metee\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\windows-targets-0.53.0\src\../readme.md
+
+C:\Users\metee\Downloads\Rust_Coding\repo_clone\target\debug\deps\windows_targets-2a42829cfd35da98.d: C:\Users\metee\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\windows-targets-0.53.0\src\lib.rs C:\Users\metee\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\windows-targets-0.53.0\src\../readme.md
+
+C:\Users\metee\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\windows-targets-0.53.0\src\lib.rs:
+C:\Users\metee\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\windows-targets-0.53.0\src\../readme.md:
